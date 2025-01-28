088d56e5232a92206c1559f93dd4b67a20daa2f9
Darlington Wleh <darlington@raddcreative.com>
2025-01-28 12:09:58 +0000

    Revert "Merge branch 'feature/LN-1369-gov-action-search-filter-and-sort-component-frontend' into 'dev'"
    This reverts merge request !9

--- ui/src/Components/Molecules/GovernanceActionCard.tsx
+++ ui/src/Components/Molecules/GovernanceActionCard.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import GovernanceActionCardHeader from "./GovernanceActionCardHeader";
 import GovernanceActionCardElement from "./GovernanceActionCardElement";
 import GovernanceActionCardIdElement from "./GovernanceActionCardIdElement";
+
 interface GovernanceActionCardProps {
   dateSubmitted: string;
   epoch: number;
@@ -27,7 +28,6 @@ function GovernanceActionCard({
   statusDate,
   statusEpoch,
 }: GovernanceActionCardProps) {
-
   return (
     <Card
       sx={{
@@ -63,16 +63,13 @@ function GovernanceActionCard({
           />
         </Box>
         <Box sx={{ marginTop: 2 }}>
-          <GovernanceActionCardIdElement
-            title="(CIP-129)Governance Action ID"
-            id={cipGovernanceActionID}
-          />
+          <GovernanceActionCardIdElement title="(CIP-129)Governance Action ID" id={cipGovernanceActionID}/>
         </Box>
         <Box display="flex" justifyContent="center" marginTop={3}>
           <Typography
             sx={{
               fontSize: "14px",
-              color: status === "Expired" ? "errorRed" : "positiveGreen",
+              color: status === "Expired" ? "#900B09" : "#29984E",
             }}
           >
             {status}:{" "}
@@ -80,7 +77,7 @@ function GovernanceActionCard({
               sx={{
                 fontSize: "14px",
                 fontWeight: "bold",
-                color: status === "Expired" ? "errorRed" : "positiveGreen",
+                color: status === "Expired" ? "#900B09" : "#29984E",
               }}
               component="span"
             >
@@ -94,8 +91,8 @@ function GovernanceActionCard({
             variant="contained"
             sx={{
               borderRadius: "50px",
-              color: "neutralWhite",
-              backgroundColor: "primaryBlue",
+              color: "#FFF",
+              backgroundColor: "#0033AD",
               width: "100%",
             }}
           >
