2747dac776213433cfcf0309c3dfb60352518b13
Cindy Joan <cindyjoan2000@gmail.com>
2025-01-30 16:05:34 +0000

    Feature/ln 1369 gov action search filter and sort component frontend

--- ui/src/Assets/Icons/UpdownArrows.tsx
+++ ui/src/Assets/Icons/UpdownArrows.tsx
@@ -17,30 +17,30 @@ export default function UpDownArrowsIcon({ width, height, color }: UpDownArrowsI
         <path
           d="M2.375 13.1666L5.54167 16.3333L8.70833 13.1666"
           stroke={color}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M5.54163 16.3333V3.66663"
           stroke={color}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.625 6.83329L13.4583 3.66663L10.2916 6.83329"
           stroke={color}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M13.4584 3.66663V16.3333"
           stroke={color}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </>
