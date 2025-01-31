ba88b5744666441073aa9d3cabeea135cd228c61
Emmanuel Titi <emmanueltiti370@gmail.com>
2025-01-31 09:15:44 +0000

    Update file GovernanceActionsList.tsx

--- ui/src/Components/Molecules/GovernanceActionsList.tsx
+++ ui/src/Components/Molecules/GovernanceActionsList.tsx
@@ -2,8 +2,8 @@ import { Box, Grid } from "@mui/material";
 import GovernanceActionCard from "./GovernanceActionCard";
 import { useGetGovernanceActions } from "../../hooks/useGetGovernanceActionsQuery";
 
-export default function GovernanceActionsList({}) {
-   const { govActions, isGovActionsLoading } = useGetGovernanceActions();
+export default function GovernanceActionsList({ }) {
+  const { govActions, isGovActionsLoading } = useGetGovernanceActions();
   return (
     <Box
       sx={{
@@ -11,7 +11,7 @@ export default function GovernanceActionsList({}) {
       }}
     >
       <Grid container spacing={{ xs: 4, sm: 6 }} sx={{ marginX: "auto" }}>
-        {!isGovActionsLoading &&
+        {!isGovActionsLoading && govActions &&
           govActions?.map((action, index) => (
             <Grid item xs={12} md={6} xl={4} key={index}>
               <GovernanceActionCard action={action} />
