b9dd329da924583967ccb0859ba3fe449d10a7d4
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-02-18 19:48:24 +0300

    feat: add governance action list pagination

--- ui/src/services/requests/getNetworkMetrics.ts
+++ ui/src/services/requests/getNetworkMetrics.ts
@@ -1,7 +1,6 @@
 import { NetworkMetrics } from "../../types/api";
 import axiosInstance from "../axiosInstance";
 
-
 export const getNetworkMetrics = async () => {
   const response = await axiosInstance.get<NetworkMetrics>("/misc/network/metrics");
   return response.data
